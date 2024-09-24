a9667e8a6752506d80c410c27293ec6de0922c39
AstroTurkey <thatturkeykid@gmail.com>
2024-09-23 15:23:51 -1000

    Add files via upload

---
+++ TankTrain.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TankTrain extends SubsystemBase {
+  
+  public TankTrain() {
+
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+
+}
